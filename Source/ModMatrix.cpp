f7a3763514d0367b4407a73b38ffca62286b4646
Frederik Siepe <FrederikSiepe@web.de>
2019-04-21 00:18:24 +0200

    forgor to add files :sweat_smile:

---
+++ Source/ModMatrix.cpp
@@ -0,0 +1,103 @@
+/*
+  ==============================================================================
+
+    ModMatrix.cpp
+    Created: 20 Apr 2019 5:04:47pm
+    Author:  frot
+
+  ==============================================================================
+*/
+
+#include "ModMatrix.h"
+
+void ModMatrixRow::setSourcesAndDestinations(ModSources *p_source,
+                                             ModDestinations *p_destination) {
+  m_sources = p_source;
+  m_destinations = p_destination;
+}
+
+void ModMatrixRow::applyModulation() {
+  // poly destination -> 12 modulations
+  if (m_destination_poly) {
+    for (int voice = 0; voice < VOICES; ++voice) {
+      if (m_scale) {
+        *(m_destination_value[voice]) +=
+            (*m_source_value[voice]) * m_mod_amount *
+            (1 + (*m_scale_value[voice] - 1) * m_scale_amount);
+      } else {
+        *(m_destination_value[voice]) +=
+            (*m_source_value[voice]) * m_mod_amount;
+      }
+    }
+  }
+}
+
+void ModMatrixRow::setModSource(int p_source) {
+  m_source = p_source;
+  m_active = (m_source && m_destination);
+}
+
+void ModMatrixRow::setModDestination(int p_destination) {
+  m_destination = p_destination;
+  m_active = (m_source && m_destination);
+}
+
+void ModMatrixRow::setModScale(int p_scale) { m_scale = p_scale; }
+
+//=========================================================
+//=================== MODMATRIX ===========================
+//=========================================================
+
+
+void ModMatrix::applyModulation() {
+  for (int row = 0; row < MODMATRIX_ROWS; ++row) {
+    if (m_row[row]) {
+      m_row[row].applyModulation();
+    }
+  }
+}
+
+void ModMatrix::setSourcesAndDestinations(ModSources *p_sources,
+                                          ModDestinations *p_destinations) {
+  for (int row = 0; row < MODMATRIX_ROWS; ++row) {
+    m_row[row].setSourcesAndDestinations(p_sources, p_destinations);
+  }
+  m_sources = p_sources;
+  m_destinations = p_destinations;
+  zeroAllDestinations();
+  zeroAllSources();
+}
+
+// ModMatrix::ModMatrix(ModSources *p_sources, ModDestinations *p_destinations){
+//  for (int row = 0; row < MODMATRIX_ROWS; ++row) {
+//    m_row[row].setSourcesAndDestinations(p_sources, p_destinations);
+//  }
+//}
+
+void ModMatrix::setModSource(int p_row, int p_source) {
+  m_row[p_row].setModSource(p_source);
+}
+
+void ModMatrix::setModDestination(int p_row, int p_destination) {
+  m_row[p_row].setModDestination(p_destination);
+}
+
+void ModMatrix::setModScale(int p_row, int p_scale) {
+  m_row[p_row].setModScale(p_scale);
+}
+
+void ModMatrix::setModAmount(int p_row, float p_mod_amount) {
+  m_row[p_row].setModAmount(p_mod_amount);
+}
+
+void ModMatrix::setScaleAmount(int p_row, float p_scale_amount) {
+  m_row[p_row].setScaleAmount(p_scale_amount);
+}
+
+void ModMatrix::zeroAllSources() {
+  memset(m_sources, 0 , sizeof(ModSources));
+}
+
+void ModMatrix::zeroAllDestinations() {
+  memset(m_destinations, 0 , sizeof(ModDestinations));  
+}
---
+++ Source/ModMatrix.h
@@ -0,0 +1,201 @@
+
+#pragma once
+#include "GlobalIncludes.h"
+#include <cstring>
+
+// contains all modulation destinations in an oscillator (all types)
+struct ModDestOsc {
+  float pitch_linear;
+  float pitch_exponential;
+  float vol;
+  // float drift;
+  float pulse_width;
+  float carrier_ratio;
+  float modulator_ratio;
+  float fm_amount;
+  float hp_freq;
+  float lp_freq;
+  float x;
+  float y;
+  float position;
+  float detune;
+  float spread;
+  float arp_speed;
+};
+
+// contains all modulation destinations in a filter (all types)
+struct ModDestFilter {
+  float freq;
+  float res;
+  float drive;
+  float gain;
+  float env_amount;
+  float vel_amount;
+  float kbd_amount;
+  float SEM_transition;
+  float formant_transition;
+};
+
+// contains all modulation destinations for an envelope
+struct ModDestADSR {
+  float attack;
+  float decay;
+  float sustain;
+  float release;
+};
+
+// contains all modulation destination within an envelope
+struct ModDestLFO {
+  float freq;
+};
+
+struct ModDestAmp {
+  float gain;
+  float pan;
+  float vel;
+};
+
+struct ModDestDistortion {
+  float threshold;
+  float drywet;
+};
+
+struct ModDestDelay {
+  float time;
+  float feedback;
+  float hp_freq;
+  float ducking;
+  float dry;
+  float wet;
+};
+
+struct ModDestPhaser {
+  float amount;
+  float freq;
+  float drywet;
+};
+
+struct ModDestFlanger {
+  float amount;
+  float freq;
+  float drywet;
+};
+
+struct ModDestChorus {
+  float amount;
+  float freq;
+  float drywet;
+};
+
+struct ModDestMisc {
+  float master;
+  float glide;
+};
+
+struct ModDestVoice {
+  ModDestOsc osc[3];
+  ModDestFilter filter[2];
+  ModDestADSR adsr[4];
+  ModDestLFO lfo[4];
+};
+
+struct ModDestinations {
+  ModDestVoice voice[VOICES];
+  ModDestAmp amp;
+  ModDestDistortion distortion;
+  ModDestFilter filter3;
+  ModDestDelay delay;
+  ModDestPhaser phaser;
+  ModDestFlanger flanger;
+  ModDestChorus chorus;
+  ModDestMisc misc;
+};
+
+struct ModSourceVoice {
+  float osc[3];
+  float filter[2];
+  float adsr[4];
+  float lfo[4];
+  float MIDI_key;
+  float MIDI_velocity;
+};
+
+struct ModSources {
+  ModSourceVoice voice[VOICES];
+  float filter3;
+  float modwheel;
+  float pitchwheel;
+  float x;
+  float y;
+  float MIDI_aftertouch;
+};
+
+class ModMatrixRow {
+public:
+  ModMatrixRow(){}
+  //ModMatrixRow(ModSources &p_source, ModSources &p_destination);
+  void setSourcesAndDestinations(ModSources* p_source, ModDestinations* p_destination);
+
+  operator bool() const {
+    return m_active;
+  }
+
+  void applyModulation();
+
+
+  void setModSource(int p_source);
+  void setModDestination(int p_destination);
+  void setModScale(int p_scale);
+
+  void setModAmount(float p_mod_amount){
+    m_mod_amount = p_mod_amount;
+  }
+
+  void setScaleAmount(float p_scale_amount){
+    m_scale_amount = p_scale_amount;
+  }
+
+private:
+  int m_source = 0;
+  int m_destination = 0;
+  int m_scale = 0;
+  bool m_active = false;
+
+  float m_mod_amount = 0.f;
+  float m_scale_amount = 0.f;
+
+  float* m_source_value[VOICES];
+  float* m_destination_value[VOICES];
+  float* m_scale_value[VOICES];
+
+  bool m_source_poly = false;
+  bool m_destination_poly = false;
+  bool m_scale_poly = false;
+
+
+  ModSources* m_sources;
+  ModDestinations* m_destinations;
+};
+
+class ModMatrix {
+public:
+  ModMatrix(){}
+  //ModMatrix(ModSources *p_source, ModDestinations *p_destination);
+
+  void setSourcesAndDestinations(ModSources *p_source, ModDestinations *p_destination);
+
+  void applyModulation();
+
+  void setModSource(int p_row, int p_source);
+  void setModDestination(int p_row, int p_destination);
+  void setModScale(int p_row, int p_scale);
+  void setModAmount(int p_row, float p_mod_amount);
+  void setScaleAmount(int p_row, float p_scale_amount);
+
+  void zeroAllSources();
+  void zeroAllDestinations();
+private:
+  ModMatrixRow m_row[MODMATRIX_ROWS];
+  ModSources* m_sources;
+  ModDestinations* m_destinations;
+};
