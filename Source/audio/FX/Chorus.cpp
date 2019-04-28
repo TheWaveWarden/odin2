848e7e4c8d3ccaf8c3576ed9f25a17763dcd17ce
Frederik Siepe <FrederikSiepe@web.de>
2019-04-28 16:23:04 +0200

    added mod destinations for chorus

--- Source/audio/FX/Chorus.cpp
+++ Source/audio/FX/Chorus.cpp
@@ -1,72 +1,103 @@
 #include "Chorus.h"
 #include <cmath>
 
+Chorus::Chorus() {}
 
+Chorus::~Chorus() {}
 
-Chorus::Chorus()
-{
-}
+float Chorus::doChorus(float p_input) {
 
+  float LFO_sine;
+  float LFO_cosine;
+  incLFO();
+  doLFO(LFO_sine, LFO_cosine);
 
-Chorus::~Chorus()
-{
-}
+  // make them unipolar
+  LFO_sine = LFO_sine * 0.5f + 0.5f;
+  LFO_cosine = LFO_cosine * 0.5f + 0.5f;
 
+  float amount_modded = m_amount + *m_amount_mod;
+  amount_modded = amount_modded < 0 ? 0 : amount_modded;
+  // amount_modded = amount_modded < 0 ? 0 : amount_modded;
 
+  // Delayline 1
+  float read_index =
+      (float)m_write_index - CHORUS_MIN_DISTANCE -
+      (LFO_sine * CHORUS_AMOUNT_RANGE * amount_modded) * m_samplerate;
+  int read_index_trunc = floor(read_index);
+  int read_index_next = read_index_trunc + 1;
+  float frac = read_index - (float)read_index_trunc;
+  read_index_trunc = read_index_trunc < 0
+                         ? read_index_trunc + CHORUS_BUFFER_LENGTH
+                         : read_index_trunc;
+  read_index_next = read_index_next < 0 ? read_index_next + CHORUS_BUFFER_LENGTH
+                                        : read_index_next;
+  read_index_next = read_index_next >= CHORUS_BUFFER_LENGTH
+                        ? read_index_next - CHORUS_BUFFER_LENGTH
+                        : read_index_next;
+  float output = linearInterpolation(m_circular_buffer[read_index_trunc],
+                                     m_circular_buffer[read_index_next], frac);
 
-float Chorus::doChorus(float p_input){
+  // Delayline 2
+  read_index =
+      (float)m_write_index - CHORUS_MIN_DISTANCE -
+      (LFO_cosine * CHORUS_AMOUNT_RANGE * amount_modded) * m_samplerate;
+  read_index_trunc = floor(read_index);
+  read_index_next = read_index_trunc + 1;
+  frac = read_index - (float)read_index_trunc;
+  read_index_trunc = read_index_trunc < 0
+                         ? read_index_trunc + CHORUS_BUFFER_LENGTH
+                         : read_index_trunc;
+  read_index_next = read_index_next < 0 ? read_index_next + CHORUS_BUFFER_LENGTH
+                                        : read_index_next;
+  read_index_next = read_index_next >= CHORUS_BUFFER_LENGTH
+                        ? read_index_next - CHORUS_BUFFER_LENGTH
+                        : read_index_next;
+  output += linearInterpolation(m_circular_buffer[read_index_trunc],
+                                m_circular_buffer[read_index_next], frac);
 
-    float LFO_sine;
-    float LFO_cosine;
-    incLFO();
-    doLFO(LFO_sine, LFO_cosine);
+  // Delayline 3
+  read_index =
+      (float)m_write_index - CHORUS_MIN_DISTANCE -
+      ((1 - LFO_sine) * CHORUS_AMOUNT_RANGE * amount_modded) * m_samplerate;
+  read_index_trunc = floor(read_index);
+  read_index_next = read_index_trunc + 1;
+  frac = read_index - (float)read_index_trunc;
+  read_index_trunc = read_index_trunc < 0
+                         ? read_index_trunc + CHORUS_BUFFER_LENGTH
+                         : read_index_trunc;
+  read_index_next = read_index_next < 0 ? read_index_next + CHORUS_BUFFER_LENGTH
+                                        : read_index_next;
+  read_index_next = read_index_next >= CHORUS_BUFFER_LENGTH
+                        ? read_index_next - CHORUS_BUFFER_LENGTH
+                        : read_index_next;
+  output += linearInterpolation(m_circular_buffer[read_index_trunc],
+                                m_circular_buffer[read_index_next], frac);
 
-    //make them unipolar
-    LFO_sine = LFO_sine * 0.5f + 0.5f;
-    LFO_cosine = LFO_cosine * 0.5f + 0.5f;
+  // Delayline 4
+  read_index =
+      (float)m_write_index - CHORUS_MIN_DISTANCE -
+      ((1 - LFO_cosine) * CHORUS_AMOUNT_RANGE * amount_modded) * m_samplerate;
+  read_index_trunc = floor(read_index);
+  read_index_next = read_index_trunc + 1;
+  frac = read_index - (float)read_index_trunc;
+  read_index_trunc = read_index_trunc < 0
+                         ? read_index_trunc + CHORUS_BUFFER_LENGTH
+                         : read_index_trunc;
+  read_index_next = read_index_next < 0 ? read_index_next + CHORUS_BUFFER_LENGTH
+                                        : read_index_next;
+  read_index_next = read_index_next >= CHORUS_BUFFER_LENGTH
+                        ? read_index_next - CHORUS_BUFFER_LENGTH
+                        : read_index_next;
+  output += linearInterpolation(m_circular_buffer[read_index_trunc],
+                                m_circular_buffer[read_index_next], frac);
 
-    //Delayline 1
-    float read_index = (float)m_write_index - CHORUS_MIN_DISTANCE - (LFO_sine * CHORUS_AMOUNT_RANGE * m_amount) * m_samplerate;
-    int read_index_trunc = floor(read_index);
-    int read_index_next = read_index_trunc + 1;
-    float frac = read_index - (float)read_index_trunc;
-    read_index_trunc = read_index_trunc < 0 ? read_index_trunc + CHORUS_BUFFER_LENGTH : read_index_trunc; 
-    read_index_next  = read_index_next  < 0 ? read_index_next  + CHORUS_BUFFER_LENGTH : read_index_next;
-    read_index_next  = read_index_next  >= CHORUS_BUFFER_LENGTH ? read_index_next  - CHORUS_BUFFER_LENGTH : read_index_next;
-    float output = linearInterpolation(m_circular_buffer[read_index_trunc], m_circular_buffer[read_index_next], frac);
+  m_circular_buffer[m_write_index] = p_input;
+  incWriteIndex();
 
-    //Delayline 2
-    read_index = (float)m_write_index - CHORUS_MIN_DISTANCE - (LFO_cosine * CHORUS_AMOUNT_RANGE * m_amount) * m_samplerate;
-    read_index_trunc = floor(read_index);
-    read_index_next = read_index_trunc + 1;
-    frac = read_index - (float)read_index_trunc;
-    read_index_trunc = read_index_trunc < 0 ? read_index_trunc + CHORUS_BUFFER_LENGTH : read_index_trunc; 
-    read_index_next  = read_index_next  < 0 ? read_index_next  + CHORUS_BUFFER_LENGTH : read_index_next;
-    read_index_next  = read_index_next  >= CHORUS_BUFFER_LENGTH ? read_index_next  - CHORUS_BUFFER_LENGTH : read_index_next;
-    output += linearInterpolation(m_circular_buffer[read_index_trunc], m_circular_buffer[read_index_next], frac);
+  float drywet_modded = m_dry_wet + *m_drywet_mod;
+  drywet_modded = drywet_modded > 1 ? 1 : drywet_modded;
+  drywet_modded = drywet_modded < 0 ? 0 : drywet_modded;
 
-    //Delayline 3
-    read_index = (float)m_write_index - CHORUS_MIN_DISTANCE - ((1 - LFO_sine) * CHORUS_AMOUNT_RANGE * m_amount) * m_samplerate;
-    read_index_trunc = floor(read_index);
-    read_index_next = read_index_trunc + 1;
-    frac = read_index - (float)read_index_trunc;
-    read_index_trunc = read_index_trunc < 0 ? read_index_trunc + CHORUS_BUFFER_LENGTH : read_index_trunc; 
-    read_index_next  = read_index_next  < 0 ? read_index_next  + CHORUS_BUFFER_LENGTH : read_index_next;
-    read_index_next  = read_index_next  >= CHORUS_BUFFER_LENGTH ? read_index_next  - CHORUS_BUFFER_LENGTH : read_index_next;
-    output += linearInterpolation(m_circular_buffer[read_index_trunc], m_circular_buffer[read_index_next], frac);
-
-    //Delayline 4
-    read_index = (float)m_write_index - CHORUS_MIN_DISTANCE - ((1 - LFO_cosine) * CHORUS_AMOUNT_RANGE * m_amount) * m_samplerate;
-    read_index_trunc = floor(read_index);
-    read_index_next = read_index_trunc + 1;
-    frac = read_index - (float)read_index_trunc;
-    read_index_trunc = read_index_trunc < 0 ? read_index_trunc + CHORUS_BUFFER_LENGTH : read_index_trunc; 
-    read_index_next  = read_index_next  < 0 ? read_index_next  + CHORUS_BUFFER_LENGTH : read_index_next;
-    read_index_next  = read_index_next  >= CHORUS_BUFFER_LENGTH ? read_index_next  - CHORUS_BUFFER_LENGTH : read_index_next;
-    output += linearInterpolation(m_circular_buffer[read_index_trunc], m_circular_buffer[read_index_next], frac);
-
-    m_circular_buffer[m_write_index] = p_input;
-    incWriteIndex();
-
-    return output * 0.25f * m_dry_wet + (1.f- m_dry_wet) * p_input;
+  return output * 0.25f * drywet_modded + (1.f - drywet_modded) * p_input;
 }
--- Source/audio/FX/Flanger.cpp
+++ Source/audio/FX/Flanger.cpp
@@ -1,30 +1,24 @@
 #include "Flanger.h"
 
+Flanger::Flanger() {}
 
+Flanger::~Flanger() {}
 
-Flanger::Flanger()
-{
-}
+float Flanger::doFlanger(float p_input) {
 
+  incrementLFO();
+  float LFO = doLFO();
 
-Flanger::~Flanger()
-{
-}
+  float amount_modded = m_LFO_amount + *m_amount_mod;
+  amount_modded = amount_modded < 0 ? 0 : amount_modded;
 
+  // todo amount mod still blows up... limit?
+  m_delay_time_control =
+      m_base_time + LFO * amount_modded * FLANGER_LFO_MAX_RANGE;
 
-float Flanger::doFlanger(float p_input){
-    
-    incrementLFO();
-    float LFO = doLFO();
+  float drywet_modded = m_dry_wet + *m_drywet_mod;
+  drywet_modded = drywet_modded < 0 ? 0 : drywet_modded;
+  drywet_modded = drywet_modded > 1 ? 1 : drywet_modded;
 
-    float amount_modded = m_LFO_amount + *m_amount_mod;
-    amount_modded = amount_modded < 0 ? 0 : amount_modded;
-
-    m_delay_time_control = m_base_time + LFO * amount_modded * FLANGER_LFO_MAX_RANGE;
-
-    float drywet_modded = m_dry_wet + *m_drywet_mod;
-    drywet_modded = drywet_modded < 0 ? 0 :drywet_modded;
-    drywet_modded = drywet_modded > 1 ? 1 :drywet_modded;
-
-    return drywet_modded * doFilter(p_input) + (1.f - drywet_modded) * p_input;
+  return drywet_modded * doFilter(p_input) + (1.f - drywet_modded) * p_input;
 }
