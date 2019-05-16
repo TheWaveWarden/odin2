71cefb4a79a1470a2b8e5fae7c358193ea2ca83a
Frederik Siepe <FrederikSiepe@web.de>
2019-05-16 23:48:13 +0200

    change in structure for wavetables

--- Source/audio/Oscillators/WavetableCoefficients.h
+++ Source/audio/Oscillators/WavetableCoefficients.h
@@ -1,81 +1,81 @@
-#include "Wavetables/Sine.h"                //0
-#include "Wavetables/ChiptuneTriangle.h"    //1
-#include "Wavetables/ChiptuneSquare50.h"    //2
-#include "Wavetables/ChiptuneSquare25.h"    //3
-#include "Wavetables/ChiptuneSquare12_5.h"  //4
-#include "Wavetables/ChiptuneCustom1.h"     //5
-#include "Wavetables/ChiptuneCustom2.h"     //6
-#include "Wavetables/ChiptuneCustom3.h"     //7
-#include "Wavetables/ChiptuneCustom4.h"     //8
-#include "Wavetables/ChiptuneCustom5.h"     //9
-#include "Wavetables/ChiptuneCustom6.h"     //10
-#include "Wavetables/Saw.h"                 //11
-#include "Wavetables/Triangle.h"            //12
-#include "Wavetables/FatSaw.h"                //13
+#include "Wavetables/Coefficients/Sine.h"                //0
+#include "Wavetables/Coefficients/ChiptuneTriangle.h"    //1
+#include "Wavetables/Coefficients/ChiptuneSquare50.h"    //2
+#include "Wavetables/Coefficients/ChiptuneSquare25.h"    //3
+#include "Wavetables/Coefficients/ChiptuneSquare12_5.h"  //4
+#include "Wavetables/Coefficients/ChiptuneCustom1.h"     //5
+#include "Wavetables/Coefficients/ChiptuneCustom2.h"     //6
+#include "Wavetables/Coefficients/ChiptuneCustom3.h"     //7
+#include "Wavetables/Coefficients/ChiptuneCustom4.h"     //8
+#include "Wavetables/Coefficients/ChiptuneCustom5.h"     //9
+#include "Wavetables/Coefficients/ChiptuneCustom6.h"     //10
+#include "Wavetables/Coefficients/Saw.h"                 //11
+#include "Wavetables/Coefficients/Triangle.h"            //12
+#include "Wavetables/Coefficients/FatSaw.h"                //13
 
-#include "Wavetables/Additive1.h"//14
-#include "Wavetables/Additive2.h"//15
-#include "Wavetables/Additive3.h"//16
-#include "Wavetables/Additive4.h"//17
-#include "Wavetables/Additive5.h"//18
-#include "Wavetables/Additive6.h"//19
-#include "Wavetables/Additive7.h"//20
-#include "Wavetables/Additive8.h"//21
-#include "Wavetables/Additive9.h"//22
-#include "Wavetables/Additive10.h"//23
-#include "Wavetables/Additive11.h"//24
-#include "Wavetables/Additive12.h"//25
-#include "Wavetables/Additive13.h"//26
-#include "Wavetables/Additive14.h"//27
-#include "Wavetables/Additive15.h"//28
-#include "Wavetables/Additive16.h"//29
+#include "Wavetables/Coefficients/Additive1.h"//14
+#include "Wavetables/Coefficients/Additive2.h"//15
+#include "Wavetables/Coefficients/Additive3.h"//16
+#include "Wavetables/Coefficients/Additive4.h"//17
+#include "Wavetables/Coefficients/Additive5.h"//18
+#include "Wavetables/Coefficients/Additive6.h"//19
+#include "Wavetables/Coefficients/Additive7.h"//20
+#include "Wavetables/Coefficients/Additive8.h"//21
+#include "Wavetables/Coefficients/Additive9.h"//22
+#include "Wavetables/Coefficients/Additive10.h"//23
+#include "Wavetables/Coefficients/Additive11.h"//24
+#include "Wavetables/Coefficients/Additive12.h"//25
+#include "Wavetables/Coefficients/Additive13.h"//26
+#include "Wavetables/Coefficients/Additive14.h"//27
+#include "Wavetables/Coefficients/Additive15.h"//28
+#include "Wavetables/Coefficients/Additive16.h"//29
 
-#include "Wavetables/Harmonics1.h"//30
-#include "Wavetables/Harmonics2.h"//31
-#include "Wavetables/Harmonics3.h"//32
-#include "Wavetables/Harmonics4.h"//33
-#include "Wavetables/Harmonics5.h"//34
-#include "Wavetables/Harmonics6.h"//35
-#include "Wavetables/Harmonics7.h"//36
-#include "Wavetables/Harmonics8.h"//37 this is overtone 1
-#include "Wavetables/Harmonics9.h"//38
-#include "Wavetables/Harmonics10.h"//39
-#include "Wavetables/Harmonics11.h"//40
-#include "Wavetables/Harmonics12.h"//41
-#include "Wavetables/Harmonics13.h"//42
-#include "Wavetables/Harmonics14.h"//43
-#include "Wavetables/Harmonics15.h"//44
-#include "Wavetables/Harmonics16.h"//45 this is overtone 8
+#include "Wavetables/Coefficients/Harmonics1.h"//30
+#include "Wavetables/Coefficients/Harmonics2.h"//31
+#include "Wavetables/Coefficients/Harmonics3.h"//32
+#include "Wavetables/Coefficients/Harmonics4.h"//33
+#include "Wavetables/Coefficients/Harmonics5.h"//34
+#include "Wavetables/Coefficients/Harmonics6.h"//35
+#include "Wavetables/Coefficients/Harmonics7.h"//36
+#include "Wavetables/Coefficients/Harmonics8.h"//37 this is overtone 1
+#include "Wavetables/Coefficients/Harmonics9.h"//38
+#include "Wavetables/Coefficients/Harmonics10.h"//39
+#include "Wavetables/Coefficients/Harmonics11.h"//40
+#include "Wavetables/Coefficients/Harmonics12.h"//41
+#include "Wavetables/Coefficients/Harmonics13.h"//42
+#include "Wavetables/Coefficients/Harmonics14.h"//43
+#include "Wavetables/Coefficients/Harmonics15.h"//44
+#include "Wavetables/Coefficients/Harmonics16.h"//45 this is overtone 8
 
-#include "Wavetables/Organ1.h"//46
-#include "Wavetables/Organ2.h"//47
-#include "Wavetables/Organ3.h"//48
-#include "Wavetables/Organ4.h"//49
+#include "Wavetables/Coefficients/Organ1.h"//46
+#include "Wavetables/Coefficients/Organ2.h"//47
+#include "Wavetables/Coefficients/Organ3.h"//48
+#include "Wavetables/Coefficients/Organ4.h"//49
 
-#include "Wavetables/BrokenSine1.h"//50
-#include "Wavetables/BrokenSine2.h"//51
-#include "Wavetables/BrokenSine3.h"//52
-#include "Wavetables/BrokenSine4.h"//53
+#include "Wavetables/Coefficients/BrokenSine1.h"//50
+#include "Wavetables/Coefficients/BrokenSine2.h"//51
+#include "Wavetables/Coefficients/BrokenSine3.h"//52
+#include "Wavetables/Coefficients/BrokenSine4.h"//53
 
-#include "Wavetables/Skyline1.h"//54
-#include "Wavetables/Skyline2.h"//55
-#include "Wavetables/Skyline3.h"//56
-#include "Wavetables/Skyline4.h"//57
+#include "Wavetables/Coefficients/Skyline1.h"//54
+#include "Wavetables/Coefficients/Skyline2.h"//55
+#include "Wavetables/Coefficients/Skyline3.h"//56
+#include "Wavetables/Coefficients/Skyline4.h"//57
 
-#include "Wavetables/Soft1.h"//58
-#include "Wavetables/Soft2.h"//59
-#include "Wavetables/Soft3.h"//60
-#include "Wavetables/Soft4.h"//61
+#include "Wavetables/Coefficients/Soft1.h"//58
+#include "Wavetables/Coefficients/Soft2.h"//59
+#include "Wavetables/Coefficients/Soft3.h"//60
+#include "Wavetables/Coefficients/Soft4.h"//61
 
-#include "Wavetables/MultiSaw1.h"//62
-#include "Wavetables/MultiSaw2.h"//63
-#include "Wavetables/MultiSaw3.h"//64
-#include "Wavetables/MultiSaw4.h"//65
+#include "Wavetables/Coefficients/MultiSaw1.h"//62
+#include "Wavetables/Coefficients/MultiSaw2.h"//63
+#include "Wavetables/Coefficients/MultiSaw3.h"//64
+#include "Wavetables/Coefficients/MultiSaw4.h"//65
 
-// #include "Wavetables/1.h"//
-// #include "Wavetables/2.h"//
-// #include "Wavetables/3.h"//
-// #include "Wavetables/4.h"//
+// #include "Wavetables/Coefficients/1.h"//
+// #include "Wavetables/Coefficients/2.h"//
+// #include "Wavetables/Coefficients/3.h"//
+// #include "Wavetables/Coefficients/4.h"//
 
 
 
@@ -103,19 +103,19 @@
 //======= LFOS =======
 //====================
 
-#include "Wavetables/LFO/LFOPyramid4.h"
-#include "Wavetables/LFO/LFOPyramid6.h"
-#include "Wavetables/LFO/LFOPyramid8.h"
-#include "Wavetables/LFO/LFOPyramid12.h"
-#include "Wavetables/LFO/LFOStair3.h"
-#include "Wavetables/LFO/LFOStair4.h"
-#include "Wavetables/LFO/LFOStair6.h"
-#include "Wavetables/LFO/LFOStair8.h"
-#include "Wavetables/LFO/LFOStair12.h"
-#include "Wavetables/LFO/LFOSquare50.h"
-#include "Wavetables/LFO/LFOSquare25.h"
-#include "Wavetables/LFO/LFOSquare12.h"
-#include "Wavetables/LFO/LFOSpike.h"
-//#include "Wavetables/LFO/ChiptuneCustom9999.h"
+#include "Wavetables/Coefficients/LFOPyramid4.h"
+#include "Wavetables/Coefficients/LFOPyramid6.h"
+#include "Wavetables/Coefficients/LFOPyramid8.h"
+#include "Wavetables/Coefficients/LFOPyramid12.h"
+#include "Wavetables/Coefficients/LFOStair3.h"
+#include "Wavetables/Coefficients/LFOStair4.h"
+#include "Wavetables/Coefficients/LFOStair6.h"
+#include "Wavetables/Coefficients/LFOStair8.h"
+#include "Wavetables/Coefficients/LFOStair12.h"
+#include "Wavetables/Coefficients/LFOSquare50.h"
+#include "Wavetables/Coefficients/LFOSquare25.h"
+#include "Wavetables/Coefficients/LFOSquare12.h"
+#include "Wavetables/Coefficients/LFOSpike.h"
+//#include "Wavetables/Coefficients/ChiptuneCustom9999.h"
 
 
