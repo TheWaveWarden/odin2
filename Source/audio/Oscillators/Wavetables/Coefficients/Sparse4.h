b2c8c30ae8a4bbacfcfd94e8a728708935ff71a7
Frederik Siepe <FrederikSiepe@web.de>
2019-06-22 21:35:16 +0200

    added wavetable files

---
+++ Source/audio/Oscillators/Wavetables/Coefficients/Sparse4.h
@@ -0,0 +1,16 @@
+#define WT_NR 105
+	
+m_wavetable_names_1D[WT_NR] = "Sparse4";
+m_fourier_coeffs[WT_NR][0][0] = 1.f; //signals sine only
+m_fourier_coeffs[WT_NR][1][0] = 0.539144; //Saw scalar (negative to invert for LFO)
+
+m_fourier_coeffs[WT_NR][0][1] = 1.0;
+m_fourier_coeffs[WT_NR][0][7] = 0.142857;
+m_fourier_coeffs[WT_NR][0][13] = 0.0769231;
+m_fourier_coeffs[WT_NR][0][19] = 0.0526316;
+m_fourier_coeffs[WT_NR][0][25] = 0.04;
+m_fourier_coeffs[WT_NR][0][31] = 0.0322581;
+m_fourier_coeffs[WT_NR][0][37] = 0.027027;
+m_fourier_coeffs[WT_NR][0][43] = 0.0232558;
+m_fourier_coeffs[WT_NR][0][49] = 0.0204082;
+#undef WT_NR
