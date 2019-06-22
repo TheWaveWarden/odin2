953e4f6e1cd2feeed4008b19c57734e3ba277eed
Frederik Siepe <FrederikSiepe@web.de>
2019-06-22 12:55:12 +0200

    fixed chiptune osc

--- Source/WavetableMappings.h
+++ Source/WavetableMappings.h
@@ -1,7 +1,8 @@
 
-#define MAP(inp, out) case inp :\
-return out;\
-break;
+#define MAP(inp, out)                                                          \
+  case inp:                                                                    \
+    return out;                                                                \
+    break;
 
 int WavetableOsc1D::wavetableMappingFM(int p_input) {
   DBG("fm");
@@ -15,10 +16,10 @@ int WavetableOsc1D::wavetableMappingFM(int p_input) {
 int WavetableOsc1D::wavetableMappingVector(int p_input) {
 
   switch (p_input) {
-    MAP(101,11)
-    MAP(102,2)
-    MAP(103,12)
-    MAP(104,0)
+    MAP(101, 11)
+    MAP(102, 2)
+    MAP(103, 12)
+    MAP(104, 0)
   default:
     DBG("ERROR: tried to map unknown wavetable " + std::to_string(p_input) +
         " for vec/fm/pm osc");
@@ -31,42 +32,18 @@ int WavetableOsc1D::wavetableMappingVector(int p_input) {
 int WavetableOsc1D::wavetableMappingChiptune(int p_input) {
   int output = 0;
   switch (p_input) {
-  case 1:
-    output = 2;
-    break;
-  case 2:
-    output = 3;
-    break;
-  case 3:
-    output = 4;
-    break;
-  case 4:
-    output = 1;
-    break;
-  case 5:
-    output = 99;
-    break;
-  case 6:
-    output = 100;
-    break;
-  case 7:
-    output = 94;
-    break;
-  case 8:
-    output = 95;
-    break;
-  case 9:
-    output = 96;
-    break;
-  case 10:
-    output = 97;
-    break;
-  case 11:
-    output = 98;
-    break;
-  case 12:
-    output = 101;
-    break;
+    MAP(1, 2)
+    MAP(2, 3)
+    MAP(3, 4)
+    MAP(4, 1)
+    MAP(5, 99)
+    MAP(6, 100)
+    MAP(7, 94)
+    MAP(8, 95)
+    MAP(9, 96)
+    MAP(10, 97)
+    MAP(11, 98)
+    MAP(12, 101)
   default:
     DBG("ERROR: tried to map unknown wavetable " + std::to_string(p_input) +
         " for chiptune osc");
