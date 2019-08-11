f56a1a24c1e45923c33703aef26d486727f04225
Frederik Siepe <FrederikSiepe@web.de>
2019-08-11 16:40:20 +0200

    adapted sed script

--- Source/audio/Oscillators/Wavetables/Coefficients/make_double_float.sh
+++ Source/audio/Oscillators/Wavetables/Coefficients/make_double_float.sh
@@ -3,10 +3,9 @@
 #bash script to make ; to f; if needed
 #USE WITH GREAT CARE ONLY. ONLY IN THIS FOLDER!!
 
-#even coeffs
-sed -i /[0]/s/";"/"f;"/g *.h
-#uneven coeffs
-sed -i /[1]/s/";"/"f;"/g *.h
+#replace for all coeffs
+sed -i /m_fourier_coeffs/s/";"/"f;"/g *.h
 #remove ff if needed
 sed -i s/ff/f/g *.h
+
 
