f8c7833308cf3f5c6494403d4c6ff93cff9b29e5
Frederik <FrederikSiepe@web.de>
2019-10-16 20:34:19 +0200

    added .clang-format file

--- Source/ModAmountComponent.cpp
+++ Source/ModAmountComponent.cpp
@@ -15,72 +15,69 @@
 
 //==============================================================================
 ModAmountComponent::ModAmountComponent() {
-  // In your constructor, you should add any child components, and
-  // initialise any special settings that your component needs.
-  // setDoubleClickReturnValue(true, 0, ModifierKeys::ctrlModifier);
-  
+	// In your constructor, you should add any child components, and
+	// initialise any special settings that your component needs.
+	// setDoubleClickReturnValue(true, 0, ModifierKeys::ctrlModifier);
 }
 
-ModAmountComponent::~ModAmountComponent() {}
+ModAmountComponent::~ModAmountComponent() {
+}
 
 void ModAmountComponent::paint(Graphics &g) {
-  /* This demo code just fills the component's background and
+	/* This demo code just fills the component's background and
      draws some placeholder text to get you started.
 
      You should replace everything in this method with your own
      drawing code..
   */
-  g.setColour(m_color);
-  juce::Point<int> top_left = getLocalBounds().getTopLeft();
-  top_left.addXY(m_inlay, m_inlay);
-  juce::Point<int> bottom_right = getLocalBounds().getBottomRight();
-  bottom_right.addXY(-m_inlay, -m_inlay);
-  g.fillRect(juce::Rectangle<int>(top_left, bottom_right)); // pmai
+	g.setColour(m_color);
+	juce::Point<int> top_left = getLocalBounds().getTopLeft();
+	top_left.addXY(m_inlay, m_inlay);
+	juce::Point<int> bottom_right = getLocalBounds().getBottomRight();
+	bottom_right.addXY(-m_inlay, -m_inlay);
+	g.fillRect(juce::Rectangle<int>(top_left, bottom_right)); // pmai
 
-  if (m_value > 0) {
-    g.setColour(m_color_bar);
-    bottom_right.addXY(-(getWidth() - m_inlay * 2) * (1.f - m_value), -m_inlay);
-    g.fillRect(juce::Rectangle<int>(top_left, bottom_right));
-  } else {
-    g.setColour(m_color_bar_negative);
-    top_left.addXY((getWidth() - m_inlay * 2) * (1 +m_value), m_inlay);
-    g.fillRect(juce::Rectangle<int>(top_left, bottom_right));
-  }
+	if (m_value > 0) {
+		g.setColour(m_color_bar);
+		bottom_right.addXY(-(getWidth() - m_inlay * 2) * (1.f - m_value), -m_inlay);
+		g.fillRect(juce::Rectangle<int>(top_left, bottom_right));
+	} else {
+		g.setColour(m_color_bar_negative);
+		top_left.addXY((getWidth() - m_inlay * 2) * (1 + m_value), m_inlay);
+		g.fillRect(juce::Rectangle<int>(top_left, bottom_right));
+	}
 
-  Font current_font = g.getCurrentFont();
-  current_font.setStyleFlags(1);//bold
-  g.setFont(current_font);
-  g.setFont(12.0f);
-  std::stringstream stream;
-  stream << std::fixed << std::setprecision(0) << m_value * 100;
-  std::string value_string = stream.str();
-  g.setColour(Colours::white);
-  if(value_string == "0"){
-    g.setColour(juce::Colours::lightgrey);
-  }
-  g.drawText(value_string, getLocalBounds(), Justification::centred,
-             true); 
+	Font current_font = g.getCurrentFont();
+	current_font.setStyleFlags(1); //bold
+	g.setFont(current_font);
+	g.setFont(12.0f);
+	std::stringstream stream;
+	stream << std::fixed << std::setprecision(0) << m_value * 100;
+	std::string value_string = stream.str();
+	g.setColour(Colours::white);
+	if (value_string == "0") {
+		g.setColour(juce::Colours::lightgrey);
+	}
+	g.drawText(value_string, getLocalBounds(), Justification::centred, true);
 
-  g.drawImageAt(m_glas_panel, 0, 0);
+	g.drawImageAt(m_glas_panel, 0, 0);
 }
 
 void ModAmountComponent::resized() {
-  // This method is where you should set the bounds of any child
-  // components that your component contains..
+	// This method is where you should set the bounds of any child
+	// components that your component contains..
 }
 
 void ModAmountComponent::mouseDrag(const MouseEvent &event) {
-  m_value = m_drag_start_value +
-            (m_drag_start_y - getMouseXYRelative().getY()) * DRAG_SCALAR;
-  m_value = m_value > 1 ? 1 : m_value;
-  m_value = m_value < -1 ? -1 : m_value;
-  onValueChange(m_value);
-  repaint();
+	m_value = m_drag_start_value + (m_drag_start_y - getMouseXYRelative().getY()) * DRAG_SCALAR;
+	m_value = m_value > 1 ? 1 : m_value;
+	m_value = m_value < -1 ? -1 : m_value;
+	onValueChange(m_value);
+	repaint();
 }
 
-
-void ModAmountComponent::parameterChanged (const String &parameterID, float newValue){
-  if(parameterID == m_parameter_id){
-    setValue(newValue);
-  }
+void ModAmountComponent::parameterChanged(const String &parameterID, float newValue) {
+	if (parameterID == m_parameter_id) {
+		setValue(newValue);
+	}
 }
