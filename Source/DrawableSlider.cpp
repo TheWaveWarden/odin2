5fab8066e0f7288a1b20ba74f5a3e5968cf4b026
Frederik Siepe <frederiksiepe@web.de>
2019-10-27 20:24:43 +0100

    formatting in DrawableSlider

--- Source/DrawableSlider.cpp
+++ Source/DrawableSlider.cpp
@@ -15,83 +15,85 @@
 OdinAudioProcessor *DrawableSlider::m_processor;
 
 //==============================================================================
-DrawableSlider::DrawableSlider()
-{
-  setLookAndFeel(&m_feels);
+DrawableSlider::DrawableSlider() {
+	setLookAndFeel(&m_feels);
 
-  setPopupDisplayEnabled(true, false, nullptr);
-  setNumDecimalPlacesToDisplay(3);
+	setPopupDisplayEnabled(true, false, nullptr);
+	setNumDecimalPlacesToDisplay(3);
 
-  m_handle = ImageCache::getFromMemory(
-      BinaryData::slider_handle_png, BinaryData::slider_handle_pngSize);
+	m_handle = ImageCache::getFromMemory(BinaryData::slider_handle_png, BinaryData::slider_handle_pngSize);
 
-  setVelocityModeParameters(1.0, 1, 0.0, true, ModifierKeys::shiftModifier);
+	setVelocityModeParameters(1.0, 1, 0.0, true, ModifierKeys::shiftModifier);
 }
 
-DrawableSlider::~DrawableSlider() { setLookAndFeel(nullptr); }
+DrawableSlider::~DrawableSlider() {
+	setLookAndFeel(nullptr);
+}
 
-void DrawableSlider::paint(Graphics &g)
-{
-  //g.setColour(Colours::grey);
-  //g.drawRect(getLocalBounds(), 1); // draw an outline around the component
+void DrawableSlider::paint(Graphics &g) {
+	//g.setColour(Colours::grey);
+	//g.drawRect(getLocalBounds(), 1); // draw an outline around the component
 
-  //DBG(getValue());
-  g.drawImageAt(m_handle, 0, (1.f - valueToProportionOfLength(getValue())) * (getHeight() - m_handle.getHeight()));
+	//DBG(getValue());
+	g.drawImageAt(m_handle, 0, (1.f - valueToProportionOfLength(getValue())) * (getHeight() - m_handle.getHeight()));
 
-  //DBG(getValue());
-  if (m_midi_learn) {
-      g.setColour(Colours::red);
-      g.drawRoundedRectangle(getLocalBounds().getX(), getLocalBounds().getY(),
-                             getLocalBounds().getWidth(),
-                             getLocalBounds().getHeight(), 5,
-                             2); // draw an outline around the component
-    }
-    else if (m_midi_control) {
-      g.setColour(Colours::green);
-      g.drawRoundedRectangle(getLocalBounds().getX(), getLocalBounds().getY(),
-                             getLocalBounds().getWidth(),
-                             getLocalBounds().getHeight(), 5,
-                             2); // draw an outline around the component
-    }
+	//DBG(getValue());
+	if (m_midi_learn) {
+		g.setColour(Colours::red);
+		g.drawRoundedRectangle(getLocalBounds().getX(),
+		                       getLocalBounds().getY(),
+		                       getLocalBounds().getWidth(),
+		                       getLocalBounds().getHeight(),
+		                       5,
+		                       2); // draw an outline around the component
+	} else if (m_midi_control) {
+		g.setColour(Colours::green);
+		g.drawRoundedRectangle(getLocalBounds().getX(),
+		                       getLocalBounds().getY(),
+		                       getLocalBounds().getWidth(),
+		                       getLocalBounds().getHeight(),
+		                       5,
+		                       2); // draw an outline around the component
+	}
 }
 
 void DrawableSlider::mouseDown(const MouseEvent &event) {
-  if (event.mods.isRightButtonDown()) {
-    //DBG("RIGHT");
-    PopupMenu midi_learn_menu;
-    if (m_midi_learn) {
-      midi_learn_menu.addItem(2, "Stop MIDI learn");
-      if (midi_learn_menu.show() == 2) {
-        stopMidiLearn();
-        m_processor->stopMidiLearn();
-      }
-    } else {
-      midi_learn_menu.addItem(2, "MIDI learn");
-      if (m_midi_control) {
-        midi_learn_menu.addItem(3, "MIDI forget");
-      }
-      int menu = midi_learn_menu.show();
-      if (menu == 2) {
-        if (m_midi_control) {
-          m_processor->midiForget(this);
-        }
-        m_processor->startMidiLearn(this);
-        m_midi_learn = true;
-        m_midi_control = false;
-        repaint();
-      } else if (menu == 3) {
-        m_processor->midiForget(this);
-        m_midi_control = false;
-        repaint();
-      }
-    }
-    return;
-  }
-  Slider::mouseDown(event);
+	if (event.mods.isRightButtonDown()) {
+		//DBG("RIGHT");
+		PopupMenu midi_learn_menu;
+		if (m_midi_learn) {
+			midi_learn_menu.addItem(2, "Stop MIDI learn");
+			if (midi_learn_menu.show() == 2) {
+				stopMidiLearn();
+				m_processor->stopMidiLearn();
+			}
+		} else {
+			midi_learn_menu.addItem(2, "MIDI learn");
+			if (m_midi_control) {
+				midi_learn_menu.addItem(3, "MIDI forget");
+			}
+			int menu = midi_learn_menu.show();
+			if (menu == 2) {
+				if (m_midi_control) {
+					m_processor->midiForget(this);
+				}
+				m_processor->startMidiLearn(this);
+				m_midi_learn   = true;
+				m_midi_control = false;
+				repaint();
+			} else if (menu == 3) {
+				m_processor->midiForget(this);
+				m_midi_control = false;
+				repaint();
+			}
+		}
+		return;
+	}
+	Slider::mouseDown(event);
 }
 
 String DrawableSlider::getTextFromValue(double value) {
-  //apparently there's a better solution:
-  //https://forum.juce.com/t/setnumdecimalplacestodisplay-not-behaving-solved/33686/2
-  return String(value, getNumDecimalPlacesToDisplay()) + getTextValueSuffix();
+	//apparently there's a better solution:
+	//https://forum.juce.com/t/setnumdecimalplacestodisplay-not-behaving-solved/33686/2
+	return String(value, getNumDecimalPlacesToDisplay()) + getTextValueSuffix();
 }
