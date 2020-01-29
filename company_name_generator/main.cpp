461f62b029796b017d36c3ec19ed3afbab290366
Frederik Siepe <frederik.siepe@robodev.eu>
2020-01-29 18:36:46 +0100

    added rune-style mainfont

--- company_name_generator/main.cpp
+++ company_name_generator/main.cpp
@@ -3,7 +3,9 @@
 
 int main(){
     
-    std::vector<std::string> list_of_names = {"henlo", "boiii"};
+    std::vector<std::string> list_of_names = {"viking", 
+    "nordic","nord","wave", "plugin", "penguin", "frost", "odin", "celtic", "forge"
+    };
 	
     for(int x = 0; x < list_of_names.size(); ++x){
         for(int y = 0; y < list_of_names.size(); ++y){
