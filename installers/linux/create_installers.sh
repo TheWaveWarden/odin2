e200bfb09d66f19e03747e210527fe79bf8a4262
Frederik Siepe <frederik.siepe@robodev.eu>
2020-07-13 10:54:04 +0200

    added warning to linux build script

--- installers/linux/create_installers.sh
+++ installers/linux/create_installers.sh
@@ -1,2 +1,5 @@
+echo "================================================================="
+echo "IMPORTANT: Build only on Ubuntu 18.4, else will give libc6 errors"
+echo "================================================================="
 bash create_deb_package.sh
 bash create_bash_installer.sh
