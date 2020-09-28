4caa9ce1138574992b6c827ae435d46a9596511e
Frederik Siepe <frederiksiepe@Alexanders-MBP.fritz.box>
2020-09-28 20:45:22 +0200

    changed permissions in mac installer prescript

--- installers/mac/migrate_soundbank_From_old_location.sh
+++ installers/mac/migrate_soundbank_From_old_location.sh
@@ -5,4 +5,5 @@ then
 	echo "old odin folder exists"
 	mkdir -p /Library/Audio/Presets/odin2
 	cp -r /Library/Audio/odin2/Soundbanks /Library/Audio/Presets/odin2/
+    chmod -R 777 /Library/Audio/odin2
 fi
