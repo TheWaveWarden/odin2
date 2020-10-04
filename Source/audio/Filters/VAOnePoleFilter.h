83171632678d5d08e18c3896be55631518f82383
Frederik Siepe <frederiksiepe@web.de>
2020-10-04 20:46:19 +0200

    cleanup in VAOnePoleFilter.h

--- Source/audio/Filters/VAOnePoleFilter.h
+++ Source/audio/Filters/VAOnePoleFilter.h
@@ -15,31 +15,49 @@
 
 #pragma once
 
-#include "Filter.h"
 #include "../../GlobalIncludes.h"
+#include "Filter.h"
 
-
-class VAOnePoleFilter : public Filter
-{
+class VAOnePoleFilter : public Filter {
 public:
 	VAOnePoleFilter();
 	virtual ~VAOnePoleFilter();
-	double m_alpha;			// Feed Forward coeff
-	double m_beta;
-	double m_gamma;		// Pre-Gain
-	double m_delta;		// FB_IN Coeff
-	double m_epsilon;		// FB_OUT scalar
-	double m_a_0;			// input gain
-	double m_feedback;		// our own feedback coeff from S 
-	void setFeedback(double fb){m_feedback = fb;}
-	double getFeedbackOutput(){return m_beta*(m_z_1 + m_feedback*m_delta);}
-	void reset(){m_z_1 = 0; m_feedback = 0;}
-	void update();
-	double doFilter(double xn);
-	inline void setLP(){m_is_lowpass = true;}
-	inline void setHP(){m_is_lowpass = false;}
-protected:
-	bool m_is_lowpass = true; //else highpass
-	double m_z_1;		// our z-1 storage location
-};
+	
+	void setFeedback(double fb) {
+		m_feedback = fb;
+	}
 
+	double getFeedbackOutput() {
+		return m_beta * (m_z_1 + m_feedback * m_delta);
+	}
+
+	void reset() {
+		m_z_1      = 0;
+		m_feedback = 0;
+	}
+
+	void update();
+
+	double doFilter(double xn);
+
+	inline void setLP() {
+		m_is_lowpass = true;
+	}
+
+	inline void setHP() {
+		m_is_lowpass = false;
+	}
+
+	//these are public for easy access
+	double m_alpha; // Feed Forward coeff
+	double m_beta;
+	double m_gamma;    // Pre-Gain
+	double m_delta;    // FB_IN Coeff
+	double m_epsilon;  // FB_OUT scalar
+	double m_a_0;      // input gain
+	double m_feedback; // our own feedback coeff from S
+protected:
+
+	bool m_is_lowpass = true; //else highpass
+	double m_z_1;             // our z-1 storage location
+};
