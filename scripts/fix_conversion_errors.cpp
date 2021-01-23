fa7f5dc2402328ecd6e64922c0c24dce47f5063f
TheWaveWarden <info@thewavewarden.com>
2021-01-23 17:59:51 +0100

    edited script again

--- scripts/fix_conversion_errors.cpp
+++ scripts/fix_conversion_errors.cpp
@@ -6,15 +6,17 @@
 
 #define DBG(text) std::cout << text << std::endl
 
-
-std::string mask_string(const std::string& p_content,const std::string& p_mask) {
+std::string mask_string(const std::string &p_content, const std::string &p_mask)
+{
     size_t end = p_content.size();
     end = p_mask.size() < end ? p_mask.size() : end;
 
     std::string ret;
 
-    for(int pos = 0; pos < end; ++pos) {
-        if(p_mask[pos] != ' ') {
+    for (int pos = 0; pos < end; ++pos)
+    {
+        if (p_mask[pos] != ' ')
+        {
             ret += p_content[pos];
         }
     }
@@ -32,27 +34,36 @@ int main()
 
     while (std::getline(ifs, line))
     {
-        if(line.find("[-Wconversion]") != std::string::npos) {
-            
-            auto file_name = line.substr(0,line.find(":"));
+        if (line.find("[-Wconversion]") != std::string::npos)
+        {
+
+            auto file_name = line.substr(0, line.find(":"));
             auto rest = line.substr(line.find(":") + 1);
             //DBG(rest);
-            auto file_line = rest.substr(0,rest.find(":"));
+            auto file_line = rest.substr(0, rest.find(":"));
 
             std::string type;
-            if(line.find("to ‘float’") != std::string::npos) {
+            if (line.find("to ‘float’") != std::string::npos)
+            {
                 type = "(float)";
-            } else if(line.find("to ‘double’") != std::string::npos) {
+            }
+            else if (line.find("to ‘double’") != std::string::npos)
+            {
                 type = "(double)";
-            } else if(line.find("to ‘int’") != std::string::npos) {
+            }
+            else if (line.find("to ‘int’") != std::string::npos)
+            {
                 type = "(int)";
-            } else if(line.find("to ‘unsigned int’") != std::string::npos) {
+            }
+            else if (line.find("to ‘unsigned int’") != std::string::npos)
+            {
                 type = "(unsigned int)";
-            } else {
+            }
+            else
+            {
                 DBG("ERROR IN LINE: " << line);
                 return 1;
             }
-
 
             std::string code_line;
             std::string squiggels_line;
@@ -70,15 +81,16 @@ int main()
             //DBG(squiggels_line);
             //DBG(problem_string);
 
-
             auto last_slash_in_filename = file_name.find_last_of("/") + 1;
             std::string pure_file_name = file_name.substr(last_slash_in_filename, file_name.size() - last_slash_in_filename);
             auto combo = pure_file_name + file_line;
 
-
-            if(std::find(edited_values.begin(), edited_values.end(), combo) != edited_values.end()) {
+            if (std::find(edited_values.begin(), edited_values.end(), combo) != edited_values.end())
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 edited_values.push_back(combo);
             }
 
@@ -86,11 +98,12 @@ int main()
             //    DBG(combo);
             //}
 
-            std::string sed_line = "sed -i '" + file_line + "s@" + problem_string + "@" + solution_string + "@' " + file_name;
+            if (problem_string.size() != 1) //weird case where only "(" would be used
+            {
+                std::string sed_line = "sed -i '" + file_line + "s@" + problem_string + "@" + solution_string + "@' " + file_name;
 
-            DBG(sed_line);
+                DBG(sed_line);
+            }
         }
     }
-
-
 }
