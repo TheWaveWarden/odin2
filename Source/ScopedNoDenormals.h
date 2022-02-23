3a1c5795587622244c0e41d53cc490961f429624
Paul Walker <paul@pwjw.com>
2022-02-23 13:03:57 -0500

    Move to CMake; build on ARM; Add clap support
    1. Add a CMakeLists.txt which works
    2. Add juce as a submodule that the above uses
    3. Fix ARM build errors
    4. Add support for CLAP via juce/cmake extensions (currently
       at clap 0.25 with this PR)

--- Source/ScopedNoDenormals.h
+++ Source/ScopedNoDenormals.h
@@ -13,7 +13,10 @@
 ** GNU General Public License for more details.
 */
 
+#if __aarch64__
+#else
 #include "xmmintrin.h"
+#endif
 
 namespace denormals {
 class ScopedNoDenormals {
@@ -21,16 +24,33 @@ public:
 	ScopedNoDenormals() {
 
 		//There is also C99 way of doing this, but its not widely supported: fesetenv(...)
+#if __aarch64__
+      uint64_t FPCR = 0;
+      asm volatile("MRS %0, FPCR " : "=r"(FPCR));
 
+      uint64_t FPCR_new = FPCR | (1ULL << 24);
+      asm volatile("MSR FPCR, %0 " : : "r"(FPCR_new));
+
+      priorS = FPCR;
+#else
 		oldMXCSR     = _mm_getcsr();      /*read the old MXCSR setting */
 		int newMXCSR = oldMXCSR | 0x8040; /* set DAZ and FZ bits */
 		_mm_setcsr(newMXCSR);             /*write the new MXCSR setting to the MXCSR */
+#endif
 	};
 
 	~ScopedNoDenormals() {
+#if __aarch64__
+      asm volatile("MSR FPCR, %0 " : : "r"(priorS));
+#else
 		_mm_setcsr(oldMXCSR);
+#endif
 	};
 
+#if __aarch64__
+  uint64_t priorS;
+#else
 	int oldMXCSR;
+#endif
 };
 } // namespace denormals
